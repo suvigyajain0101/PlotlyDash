1ab776f9226b5f9277f2b1b07cc55ae2a0991ba8
Suvigya Jain <42478893+suvigyajain0101@users.noreply.github.com>
2022-05-27 12:01:03 +0100

    Update templates.py
    Added expected outputs

--- GoldPricesApp/templates.py
+++ GoldPricesApp/templates.py
@@ -1,2 +1,9 @@
 import plotly.io as pio
 print(pio.templates)
+
+# Output from the above script - 
+# Default template: 'plotly'
+#     Available templates:
+#         ['ggplot2', 'seaborn', 'simple_white', 'plotly',
+#          'plotly_white', 'plotly_dark', 'presentation', 'xgridoff',
+#          'ygridoff', 'gridon', 'none']
